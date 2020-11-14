5561797e6a11b776cc9f74044b6246403a8342d5
adlamas <alan12394@hotmail.com>
2020-11-14 01:39:13 -0300

    agregado el metodo para sacar las stopwords

--- filtrador.py
+++ filtrador.py
@@ -1,4 +1,14 @@
+from nltk.corpus import stopwords
+
 class Filtrador:
+
+    def __sacar_stopwords(self, array_palabras):
+        sw = stopwords.words('spanish')
+        nuevo_array = []
+        for palabra in array_palabras:
+            if palabra not in sw:
+                nuevo_array.append(palabra)
+        return nuevo_array
 
     def frase_completa(self, resultados_sin_filtrar, frase):
         resultado_total_filtrado = []
@@ -11,6 +21,7 @@ class Filtrador:
     def contenga_todas_las_palabras(self, resultados_sin_filtrar, frase):
         resultado_total_filtrado = []
         array_palabras = frase.split()
+        array_palabras = self.__sacar_stopwords(array_palabras)
         for arreglo_prod in resultados_sin_filtrar:
             guardia = True
             for palabra in array_palabras:
@@ -26,6 +37,7 @@ class Filtrador:
     def contenga_algunas_palabras(self, resultados_sin_filtrar, frase):
         resultado_total_filtrado = []
         array_palabras = frase.split()
+        array_palabras = self.__sacar_stopwords(array_palabras)
         for arreglo_prod in resultados_sin_filtrar:
             guardia = False
             for palabra in array_palabras:
